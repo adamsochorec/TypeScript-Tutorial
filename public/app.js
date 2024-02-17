e60e6a4dd0ef9cfd39d5a788912693bbfd8383a7
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-02-17 19:27:01 +0100

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,8 +1,35 @@
-"use strict";
-// const anchor = document.querySelector("a")!;
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-// console.log(anchor.href);
+const me = {
+    name: "shaun",
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log("I spent", amount);
+        return amount;
+    },
+};
+let greetPerson = (person) => {
+    console.log("hellou", person.name);
+};
+greetPerson(me);
+console.log(me);
+import { Invoice } from "./classes/invoice.js";
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 350);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector(".new-item-form");
-console.log(form.children);
+// inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,53 @@
-// const anchor = document.querySelector("a")!;
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
 
-// if (anchor) {
-//   console.log(anchor.href);
-// }
+const me: IsPerson = {
+  name: "shaun",
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spent", amount);
+    return amount;
+  },
+};
 
-// console.log(anchor.href);
+let greetPerson = (person: IsPerson) => {
+  console.log("hellou", person.name);
+};
+greetPerson(me);
+
+console.log(me);
+
+import { Invoice } from "./classes/invoice.js";
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 350);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-console.log(form.children);
+
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLSelectElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
